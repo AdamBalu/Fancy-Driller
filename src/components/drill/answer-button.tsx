66dda29e15ba448391e20a96c5bc74ea0c6295bc
AdamBalu <a.baluseskul@gmail.com>
2024-06-05 09:46:08 +0200

    feat: fast mode

--- src/components/drill/answer-button.tsx
+++ src/components/drill/answer-button.tsx
@@ -1,38 +1,66 @@
 import Button from "~/components/common/button";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { type QuestionExtendedInfo } from "~/types/question";
+import { QuestionContext } from "~/hooks/question-context";
 
 type AnswerButtonProps = {
   answer: string;
   selectedAnswersState: [string[], (value: string[]) => void];
   currentQuestion: QuestionExtendedInfo;
+  onNextQuestionClick: () => void;
 };
 
 export const AnswerButton = ({
   answer,
   selectedAnswersState,
   currentQuestion,
+  onNextQuestionClick,
 }: AnswerButtonProps) => {
+  const questionContext = useContext(QuestionContext);
   const [selectedAnswers, setSelectedAnswers] = selectedAnswersState;
   const [selected, setSelected] = useState(false);
+  if (questionContext === null) {
+    return;
+  }
+  const onClick = () => {
+    const newSelectedAnswers = [...selectedAnswers, answer];
+    if (!selected) {
+      setSelectedAnswers(newSelectedAnswers);
+    } else {
+      setSelectedAnswers(selectedAnswers.filter((item) => item !== answer));
+    }
+    setSelected(!selected);
+
+    if (questionContext.fastMode) {
+      const isAnswerWrong = newSelectedAnswers.some((answer) =>
+        currentQuestion.wrong.includes(answer),
+      );
+      const isAnswerCorrect =
+        !isAnswerWrong &&
+        currentQuestion.correct.every((correctAnswer) =>
+          newSelectedAnswers.includes(correctAnswer),
+        );
+
+      if (isAnswerCorrect) {
+        questionContext.setQuestion(currentQuestion, "correct");
+        onNextQuestionClick();
+      } else if (isAnswerWrong) {
+        questionContext.setQuestion(currentQuestion, "wrong");
+        onNextQuestionClick();
+      }
+    }
+  };
 
   return (
     <Button
-      onClick={() => {
-        if (!selected) {
-          setSelectedAnswers([...selectedAnswers, answer]);
-        } else {
-          setSelectedAnswers(selectedAnswers.filter((item) => item !== answer));
-        }
-        setSelected(!selected);
-      }}
+      onClick={onClick}
       className={`!w-full !max-w-screen-xl ${
         selected
           ? "border-correct !text-correct hover:border-correct " +
             "hover:text-correct"
           : "bg-transparent"
-      } ${currentQuestion.correct.includes(answer) && currentQuestion.answer !== "none" && "!bg-correctCard hover:border-correctCard border-correctCard !text-white hover:text-white"} 
-        ${currentQuestion.wrong.includes(answer) && currentQuestion.answer !== "none" && "!bg-incorrectCard hover:border-incorrectCard border-incorrectCard !text-white hover:text-white"}`}
+      } ${currentQuestion.correct.includes(answer) && currentQuestion.answer !== "none" && "border-correctCard !bg-correctCard !text-white hover:border-correctCard hover:text-white"} 
+        ${currentQuestion.wrong.includes(answer) && currentQuestion.answer !== "none" && "border-incorrectCard !bg-incorrectCard !text-white hover:border-incorrectCard hover:text-white"}`}
     >
       {answer}
     </Button>
